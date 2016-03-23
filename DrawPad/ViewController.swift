1dff54f5c5c69f775d202919c60e1fcc95b1fcb6
Serge Panev <panev_s@epita.fr>
2016-03-23 01:14:28 +0100

    Handle exception on setViewController in the PageVC

--- DrawPad/ViewController.swift
+++ DrawPad/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  // MARK: - IBOutlet
   @IBOutlet weak var mainImageView: UIImageView!
   @IBOutlet weak var tempImageView: UIImageView!
 
+  // MARK: - Prorieties
+  var lastPoint = CGPoint.zero
+  var red: CGFloat = 0.0
+  var green: CGFloat = 0.0
+  var blue: CGFloat = 0.0
+  var brushWidth: CGFloat = 10.0
+  var opacity: CGFloat = 1.0
+  var swiped: Bool = false
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -23,8 +33,58 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    swiped = false
+    if let touch = touches.first {
+      lastPoint = touch.locationInView(self.view)
+      debugPrint(lastPoint)
+    }
+  }
+  
+  // MARK: - Drawing
+  
+  func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+    
+    // 1
+    UIGraphicsBeginImageContext(view.frame.size)
+    let context = UIGraphicsGetCurrentContext()
+    tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+    
+    // 2
+    CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+    CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+    
+    // 3
+    CGContextSetLineCap(context, CGLineCap.Round)
+    
+    CGContextSetLineWidth(context, brushWidth)
+    CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+    CGContextSetBlendMode(context, CGBlendMode.Normal)
+    
+    // 4
+    CGContextStrokePath(context)
+    
+    // 5
+    tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+    tempImageView.alpha = opacity
+    UIGraphicsEndImageContext()
+    
+  }
+  
+  override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+    // 6
+    swiped = true
+    if let touch = touches.first as? UITouch {
+      let currentPoint = touch.locationInView(view)
+      drawLineFrom(lastPoint, toPoint: currentPoint)
+      
+      // 7
+      lastPoint = currentPoint
+    }
+  }
+  
   // MARK: - Actions
-
+  
   @IBAction func reset(sender: AnyObject) {
   }
 
@@ -33,5 +93,6 @@ class ViewController: UIViewController {
   
   @IBAction func pencilPressed(sender: AnyObject) {
   }
+  
 }
 
